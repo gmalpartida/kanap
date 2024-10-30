414450149e0d9be4f876fc83610bcf6c2d64b1d7
gino malpartida <gino.malpartida@pepsico.com>
2024-10-29 21:37:33 -0400

    complete implementation of product page

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', get_product);
+document.getElementById('addToCart').addEventListener('click', add_to_cart);
 
 async function get_product(){
     const product_id = get_product_id();
@@ -14,7 +15,6 @@ async function get_product(){
             throw new Error('Response Status: $(response.status)');
         }
         const product = await response.json();
-        console.log(product);
         populate_page(product);
     }
     catch(error){
@@ -27,6 +27,66 @@ function get_product_id(){
     return params.get('id');
 }
 
-function populate_page(product){
+function create_img_element(product){
+    img = document.createElement('img');
+    img.src = product.imageUrl;
+    img.alt = product.altTxt;
+    return img;
+}
 
+function populate_colors(product){
+    select = document.getElementById('colors');
+    for (let i = 0; i < product.colors.length; i++){
+        option = document.createElement('option');
+        option.value = product.colors[i];
+        option.innerHTML = product.colors[i];
+        select.appendChild(option);
+    }
+}
+
+function populate_page(product){
+    img = create_img_element(product);
+    img_div = document.getElementsByClassName('item__img');
+    img_div[0].appendChild(img);
+
+    h1 = document.getElementById('title');
+    h1.textContent = product.name;
+
+    span = document.getElementById('price');
+    span.textContent = product.price;
+
+    p = document.getElementById('description');
+    p.textContent = product.description;
+
+    populate_colors(product);
+}
+
+function add_to_cart(){
+    product_id = get_product_id();
+    color = document.getElementById('colors').value;
+    quantity = document.getElementById('quantity').value;
+
+    if (0 >= quantity){
+        alert('Please, select at least 1 item.');
+        document.getElementById('quantity').focus();
+    }
+    else if (color == ''){
+        alert('Please select a color.');
+        document.getElementById('colors').focus();
+    }
+    else{
+        key = product_id + color;
+
+        product_in_cart = JSON.parse(localStorage.getItem(key));
+
+        if (null == product_in_cart){
+            product_in_cart = {'product_id':product_id, 'color':color, 'quantity':quantity};
+        }
+        else{
+            product_in_cart.quantity = parseInt(product_in_cart.quantity) + parseInt(quantity);
+        }   
+
+        localStorage.setItem(key, JSON.stringify(product_in_cart));    
+        console.log(localStorage);
+    }
 }
