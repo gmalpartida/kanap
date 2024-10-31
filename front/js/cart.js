7d2d4f2e9d8f29b4d85baa0c1fa3cc61f401f481
gino malpartida <gino.malpartida@pepsico.com>
2024-10-30 22:53:37 -0400

    implement cart list in cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,8 +3,8 @@ window.addEventListener('load', populate_page);
 async function populate_page(){
     console.log('cart.js populate_page')
     for (let i = 0; i < localStorage.length; i++){
-        product_key = localStorage.key(i);
-        product_in_cart = JSON.parse(localStorage.getItem(product_key));
+        let product_key = localStorage.key(i);
+        let product_in_cart = JSON.parse(localStorage.getItem(product_key));
 
         const response = await fetch('http://localhost:3000/api/products/' + product_in_cart.product_id, {
             method: 'GET',
@@ -18,13 +18,11 @@ async function populate_page(){
 
         populate_cart(product_in_cart, product);
 
-
     }
-
 }
 
 function create_article_element(product_in_cart, product){
-    article = document.createElement('article');
+    let article = document.createElement('article');
     article.classList.add('cart__item');
     article.setAttribute('data-id', product_in_cart.product_id);
     article.setAttribute('data-color', product_in_cart.color)
@@ -32,21 +30,23 @@ function create_article_element(product_in_cart, product){
 }
 
 function create_cart_item_img_element(product){
-    div = document.createElement('div');
+    let div = document.createElement('div');
     div.classList.add('cart__item__img');
-    img = document.createElement('img');
+    let img = document.createElement('img');
     img.src = product.imageUrl;
     div.appendChild(img);
     return div;
 }
 
 function create_cart_item_content_description_element(product_in_cart, product){
-    div = document.createElement('div');
+    let div = document.createElement('div');
     div.classList.add('cart__item__content__description');
-    h2_name = document.createElement('h2');
+    let h2_name = document.createElement('h2');
     h2_name.textContent = product.name;
-    p_color = document.createElement('p');
-    p_price = document.createElement('p');
+    let p_color = document.createElement('p');
+    p_color.textContent = product_in_cart.color;
+    let p_price = document.createElement('p');
+    p_price.textContent = '€' + product.price;
 
     div.appendChild(h2_name);
     div.appendChild(p_color);
@@ -56,26 +56,77 @@ function create_cart_item_content_description_element(product_in_cart, product){
 }
 
 function create_cart_item_content(product_in_cart, product){
-    div = document.createElement('div');
+    let div = document.createElement('div');
     div.classList.add('cart__item__content');
-
     description_element = create_cart_item_content_description_element(product_in_cart, product);
-
+    let cart_item_content_settings = create_cart_item_content_setting_element(product_in_cart, product);
     div.appendChild(description_element);
+    div.appendChild(cart_item_content_settings);
 
     return div;
 }
 
+function create_cart_item_content_setting_element(product_in_cart, product){
+    //<div class="cart__item__content__settings">
+    let cart_item_content_settings = document.createElement('div');
+    cart_item_content_settings.classList.add('cart__item__content__settings');
+
+    //<div class="cart__item__content__settings__quantity">
+    let cart_item_content_settings_quantity = document.createElement('div');
+    cart_item_content_settings_quantity.classList.add('cart__item__content__settings__quantity');
+    cart_item_content_settings.appendChild(cart_item_content_settings_quantity);
+
+    //<p>Quantity : </p>
+    let quantity = document.createElement('p');
+    quantity.textContent = 'Quantity : ';
+    cart_item_content_settings_quantity.appendChild(quantity);
+
+    //<input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+    let input_element = document.createElement('input');
+    input_element.classList.add('itemQuantity');
+    input_element.setAttribute('type', 'number');
+    input_element.setAttribute('name', 'itemQuantity');
+    input_element.setAttribute('min', '1');
+    input_element.setAttribute('max', '100');
+    input_element.setAttribute('value', product_in_cart.quantity);
+    cart_item_content_settings_quantity.appendChild(input_element);
+
+    //<div class="cart__item__content__settings__delete">
+      //  <p class="deleteItem">Delete</p>
+    //</div>
+    let cart_item_content_settings_delete = document.createElement('div');
+    cart_item_content_settings_delete.classList.add('cart__item__content__settings__delete');    
+    cart_item_content_settings.appendChild(cart_item_content_settings_delete);
+    cart_item_content_settings_delete.addEventListener('click', delete_cart_item);
+
+    let delete_item = document.createElement('p');
+    delete_item.classList.add('deleteItem');
+    delete_item.textContent = 'Delete';
+    cart_item_content_settings_delete.appendChild(delete_item);
+
+
+    return cart_item_content_settings;
+}
+
+function delete_cart_item(){
+    console.log(this);
+    let product_id = this.parentNode.parentNode.parentNode.getAttribute('data-id');
+    let product_color = this.parentNode.parentNode.parentNode.getAttribute('data-color');
+    console.log(this.parentNode.parentNode.parentNode);
+    key = product_id + product_color;
+
+    localStorage.removeItem(key);
+    location.reload();
+}
+
 function populate_cart(product_in_cart, product){
-    console.log(product_in_cart);
-    console.log(product);
 
-    article = create_article_element(product_in_cart, product);
+    let article = create_article_element(product_in_cart, product);
 
-    cart_item_img = create_cart_item_img_element(product);
-    cart_item_content = create_cart_item_content(product_in_cart, product);
+    let cart_item_img = create_cart_item_img_element(product);
+    let cart_item_content = create_cart_item_content(product_in_cart, product);
 
-    cart_items = document.getElementById('cart__items');
+    let cart_items = document.getElementById('cart__items');
 
     cart_items.appendChild(article);
 
